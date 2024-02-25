fd70335374c22d8c036d4bcf1c800b3f0f14e069
Chiarlini Chloé <chloe@onicorn.com>
2024-02-25 17:02:40 +0100

    🎉 init

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from "./Header.svelte";
+  import "./output.css";
+</script>
+
+<div class="bg-red-400">
+  <p>test</p>
+  <main>
+    <slot />
+  </main>
+
+  <footer></footer>
+</div>
